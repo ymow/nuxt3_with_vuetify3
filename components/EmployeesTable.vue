1c03e78b05b9ba188646f347305d50dffe18f244
Ymow Wu <kenshin0812@hotmail.com>
2022-09-23 15:10:06 +0800

    add table on click event for vuetify3

---
+++ components/EmployeesTable.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-table theme="light">
+    <thead>
+      <tr>
+        <th v-for="item in headers" :key="item.id" class="text-left">
+          {{item.text}}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in employees" :key="item.id" :items-per-page="5" class="elevation-1" @click="selectRow(item)">
+        <td>{{ item.id }}</td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.title }}</td>
+        <td>{{ item.salary }}</td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
+
+<script>
+export default {
+  name: 'EmployeesTable',
+  props: {
+    employees: {
+      type: Array,
+      required: true
+    }
+  },
+  data: () => ({
+    headers: [
+      { text: 'Employee ID', value: 'id' },
+      { text: 'Name', value: 'name' },
+      { text: 'Position Title', value: 'title' },
+      { text: 'Salary', value: 'salary' }
+    ]
+  }),
+  methods: {
+    selectRow: function (employee) {
+      this.$emit('select-employee', employee)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/signup.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-app>
+        <v-card>
+            <v-card-title>
+                <h1>Login</h1>
+            </v-card-title>
+            <v-card-text>
+                <v-form>
+                    <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+                    <v-text-field type="Password" label="Password" prepend-icon="mdi-lock" append-icon="mdi-eye-off" />
+                </v-form>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+                <v-btn color="success">Register</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn color="info">Login</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-app>
+</template>
+<script>
+export default {
+    data: () => ({
+        links: [
+            'Dashboard',
+            'Messages',
+            'Profile',
+            'Updates',
+        ],
+    }),
+}
+</script>
